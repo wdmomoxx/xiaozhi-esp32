bc8af5d15154129ae27814bb222056f1a39ed35b
wdmomoxx <540825306@qq.com>
2025-03-20 11:58:41 +0800

    Update esp32_cgc_144_board.cc
    背光控制已合并到Screen内，删除原来的背光控制器

--- main/boards/esp32-cgc-144/esp32_cgc_144_board.cc
+++ main/boards/esp32-cgc-144/esp32_cgc_144_board.cc
@@ -139,7 +139,6 @@ private:
     void InitializeIot() {
         auto& thing_manager = iot::ThingManager::GetInstance();
         thing_manager.AddThing(iot::CreateThing("Screen"));
-        thing_manager.AddThing(iot::CreateThing("Backlight"));
         thing_manager.AddThing(iot::CreateThing("BoardControl"));
         thing_manager.AddThing(iot::CreateThing("Battery"));
     }
