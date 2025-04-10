af06a75078e49a1eba13109aa540f70ac6df6e77
wdmomoxx <540825306@qq.com>
2025-04-10 15:36:05 +0800

    Update board_control.cc
    增加ESP32_CGC_144_lite选项

--- main/boards/esp32-cgc-144/board_control.cc
+++ main/boards/esp32-cgc-144/board_control.cc
@@ -17,17 +17,6 @@ namespace iot {
 class BoardControl : public Thing {
 public:
     BoardControl() : Thing("BoardControl", "当前 AI 机器人管理和控制") {
-	
-        // // 修改重新配网
-        // methods_.AddMethod("ResetWifiConfiguration", "重新配网", ParameterList(), 
-        //     [this](const ParameterList& parameters) {
-        //         ESP_LOGI(TAG, "ResetWifiConfiguration");
-        //         auto board = static_cast<WifiBoard*>(&Board::GetInstance());
-        //         if (board && board->GetBoardType() == "wifi") {
-        //             board->ResetWifiConfiguration();
-        //         }
-        //     });
-
         // 修改音量调节
         properties_.AddNumberProperty("volume", "当前音量值", [this]() -> int {
             auto codec = Board::GetInstance().GetAudioCodec();
@@ -35,18 +24,34 @@ public:
         });
 
         // 定义设备可以被远程执行的指令
+        #if defined(ESP32_CGC_144_lite)
         methods_.AddMethod("SetVolume", "设置音量", ParameterList({
             Parameter("volume", "0到100之间的整数", kValueTypeNumber, true)
         }), [this](const ParameterList& parameters) {
             auto codec = Board::GetInstance().GetAudioCodec();
             // 获取传入的音量值
             int volume = parameters["volume"].number();
-            // 限制音量值不超过66
+            // 限制音量值
             if (volume > 66) {
                 volume = 66;
             }
             codec->SetOutputVolume(static_cast<uint8_t>(volume));
         });
+        #else
+        methods_.AddMethod("SetVolume", "设置音量", ParameterList({
+            Parameter("volume", "0到100之间的整数", kValueTypeNumber, true)
+        }), [this](const ParameterList& parameters) {
+            auto codec = Board::GetInstance().GetAudioCodec();
+            // 获取传入的音量值
+            int volume = parameters["volume"].number();
+            // 限制音量值
+            if (volume > 100) {
+                volume = 100;
+            }
+            codec->SetOutputVolume(static_cast<uint8_t>(volume));
+        });
+        #endif
+
     }
 };
 
