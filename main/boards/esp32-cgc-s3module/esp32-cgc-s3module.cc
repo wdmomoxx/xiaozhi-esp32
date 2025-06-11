d0a394b44237088adae805cefefb55885db13f26
wdmomoxx <540825306@qq.com>
2025-06-11 09:23:27 +0800

    Add files via upload

---
+++ main/boards/esp32-cgc-s3module/esp32-cgc-s3module.cc
@@ -0,0 +1,177 @@
+#include "wifi_board.h"
+#include "audio_codecs/es8311_audio_codec.h"
+#include "display/lcd_display.h"
+#include "application.h"
+#include "button.h"
+#include "config.h"
+#include "i2c_device.h"
+#include "mcp_server.h"
+#include "lamp_controller.h"
+#include "iot/thing_manager.h"
+#include "led/single_led.h"
+#include "assets/lang_config.h"
+
+#include <wifi_station.h>
+#include <esp_log.h>
+#include <esp_efuse_table.h>
+#include <driver/i2c_master.h>
+
+#include <esp_lcd_panel_vendor.h>
+#include <esp_lcd_panel_io.h>
+#include <esp_lcd_panel_ops.h>
+
+
+#define TAG "ESP32_CGC_S3Module"
+
+LV_FONT_DECLARE(font_puhui_16_4);
+LV_FONT_DECLARE(font_awesome_16_4);
+
+class ESP32_CGC_S3Module : public WifiBoard {
+private:
+    i2c_master_bus_handle_t codec_i2c_bus_;
+    Button boot_button_;
+    LcdDisplay* display_;
+	
+    void InitializeCodecI2c() {
+        // Initialize I2C peripheral
+        i2c_master_bus_config_t i2c_bus_cfg = {
+            .i2c_port = I2C_NUM_0,
+            .sda_io_num = AUDIO_CODEC_I2C_SDA_PIN,
+            .scl_io_num = AUDIO_CODEC_I2C_SCL_PIN,
+            .clk_source = I2C_CLK_SRC_DEFAULT,
+            .glitch_ignore_cnt = 7,
+            .intr_priority = 0,
+            .trans_queue_depth = 0,
+            .flags = {
+                .enable_internal_pullup = 1,
+            },
+        };
+        ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_));
+    }
+
+	
+    void InitializeST7789_80_Display() {
+        esp_lcd_panel_io_handle_t panel_io = nullptr;
+        esp_lcd_panel_handle_t panel = nullptr;
+        esp_lcd_i80_bus_handle_t i80_bus = NULL;
+        esp_lcd_i80_bus_config_t bus_config = {
+            .dc_gpio_num = LCD_NUM_DC,
+            .wr_gpio_num = LCD_NUM_WR,
+            .clk_src = LCD_CLK_SRC_DEFAULT,
+            .data_gpio_nums = {
+                GPIO_LCD_D0,
+                GPIO_LCD_D1,
+                GPIO_LCD_D2,
+                GPIO_LCD_D3,
+                GPIO_LCD_D4,
+                GPIO_LCD_D5,
+                GPIO_LCD_D6,
+                GPIO_LCD_D7,
+            },
+            .bus_width = 8,
+            .max_transfer_bytes = DISPLAY_WIDTH * DISPLAY_HEIGHT * sizeof(uint16_t),
+            .psram_trans_align = 64,
+            .sram_trans_align = 4,
+        };
+        ESP_ERROR_CHECK(esp_lcd_new_i80_bus(&bus_config, &i80_bus));
+
+        esp_lcd_panel_io_i80_config_t io_config = {
+            .cs_gpio_num = LCD_NUM_CS,
+            .pclk_hz = (10 * 1000 * 1000),
+            .trans_queue_depth = 10,
+            .on_color_trans_done = nullptr,
+            .user_ctx = nullptr,
+            .lcd_cmd_bits = 8,
+            .lcd_param_bits = 8,
+            .dc_levels = {
+                .dc_idle_level = 0,
+                .dc_cmd_level = 0,
+                .dc_dummy_level = 0,
+                .dc_data_level = 1,
+            },
+            .flags = {
+                .swap_color_bytes = 0,
+            },
+        };
+        ESP_ERROR_CHECK(esp_lcd_new_panel_io_i80(i80_bus, &io_config, &panel_io));
+
+        esp_lcd_panel_dev_config_t panel_config = {
+            .reset_gpio_num = LCD_NUM_RST,
+            .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
+            .bits_per_pixel = 16,
+        };
+        ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(panel_io, &panel_config, &panel));
+
+        esp_lcd_panel_reset(panel);
+        esp_lcd_panel_init(panel);
+        esp_lcd_panel_invert_color(panel, true);
+        esp_lcd_panel_set_gap(panel, 0, 0);
+        uint8_t data0[] = {0x00};
+        uint8_t data1[] = {0x65};
+        esp_lcd_panel_io_tx_param(panel_io, 0x36, data0, 1);
+        esp_lcd_panel_io_tx_param(panel_io, 0x3A, data1, 1);
+        esp_lcd_panel_swap_xy(panel, DISPLAY_SWAP_XY);
+        esp_lcd_panel_mirror(panel, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
+        ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel, true));
+
+        display_ = new SpiLcdDisplay(panel_io, panel,
+                                    DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY,
+                                    {
+                                        .text_font = &font_puhui_16_4,
+                                        .icon_font = &font_awesome_16_4,
+                                        .emoji_font = font_emoji_32_init(),
+                                    });
+    }
+
+
+ 
+    void InitializeButtons() {
+        boot_button_.OnClick([this]() {
+            auto& app = Application::GetInstance();
+            if (app.GetDeviceState() == kDeviceStateStarting && !WifiStation::GetInstance().IsConnected()) {
+                ResetWifiConfiguration();
+            }
+            app.ToggleChatState();
+        });
+    }
+
+    // 物联网初始化，添加对 AI 可见设备
+    void InitializeIot() {
+        auto& thing_manager = iot::ThingManager::GetInstance();
+        thing_manager.AddThing(iot::CreateThing("Speaker"));
+        thing_manager.AddThing(iot::CreateThing("Screen"));
+    }
+
+public:
+    ESP32_CGC_S3Module() :
+        boot_button_(BOOT_BUTTON_GPIO) {
+        InitializeCodecI2c();
+        InitializeST7789_80_Display();
+        InitializeButtons();
+        InitializeIot();
+        GetBacklight()->RestoreBrightness();
+    }
+
+    virtual Led* GetLed() override {
+        static SingleLed led_strip(BUILTIN_LED_GPIO);
+        return &led_strip;
+    }
+
+    virtual Display* GetDisplay() override {
+        return display_;
+    }
+    
+    virtual Backlight* GetBacklight() override {
+        static PwmBacklight backlight(DISPLAY_BACKLIGHT_PIN, DISPLAY_BACKLIGHT_OUTPUT_INVERT);
+        return &backlight;
+    }
+
+    virtual AudioCodec* GetAudioCodec() override {
+        static Es8311AudioCodec audio_codec(codec_i2c_bus_, I2C_NUM_0, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_GPIO_MCLK, AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN,
+            AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_ADDR);
+        return &audio_codec;
+    }
+};
+
+DECLARE_BOARD(ESP32_CGC_S3Module);
